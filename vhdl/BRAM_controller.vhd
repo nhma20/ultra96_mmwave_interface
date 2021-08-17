11d703e20d0ceb14b45a91775f33229584d932f5
nhma20 <76950970+nhma20@users.noreply.github.com>
2021-08-17 11:37:51 +0200

    read all from RAM, write to BRAM

--- vhdl/BRAM_controller.vhd
+++ vhdl/BRAM_controller.vhd
@@ -34,14 +34,15 @@ use IEEE.NUMERIC_STD.ALL;
 entity BRAM_controller is
         port (
         ----  STD ports:
-        --clk     	:   in  STD_LOGIC;
+        i_Clk     	:   in  STD_LOGIC;
         --rst     	:   in  STD_LOGIC;
 
         --  Data in ports:
         data_in     :   in  STD_LOGIC_VECTOR(127 downto 0);
-        --channel_in  :   in  STD_LOGIC_VECTOR(1 downto 0);
         irq_in      :   in  STD_LOGIC;
-
+        i_num_points:   in std_logic_vector(4 downto 0);
+        o_address   :   out std_logic_vector(4 downto 0);
+        
         --  BRAM ports:
         bram_addr   :   out STD_LOGIC_VECTOR(31 downto 0);
         bram_dout   :   out STD_LOGIC_VECTOR(31 downto 0);
@@ -54,12 +55,85 @@ entity BRAM_controller is
 end BRAM_controller;
 
 architecture Behavioral of BRAM_controller is
+    signal ena_shift_reg    :   std_logic_vector(1 downto 0) := "00";
+    signal s_address_out    :   std_logic_vector(4 downto 0) := "00000";
+    signal s_bram_wr        :   std_logic_vector(3 downto 0) := "0000";
+    signal s_data_out       :   std_logic_vector(127 downto 0) := (others => '0');
 begin
-    bram_addr(31 downto 4)  <=  (30 => '1', others => '0');
-    bram_addr(3 downto 0)   <=  "0000"; --channel_in & "00";
+
+    ------------------------------------------------------------------------------
+    fill_BRAM: process(i_Clk, irq_in, data_in)
+    ------------------------------------------------------------------------------
+    -- Reads data from RAM and stores it in BRAM.
+    -- Only x-values are stored since BRAM is limited to 32 bit data width
+    ------------------------------------------------------------------------------
+     constant rising    :   std_logic_vector(1 downto 0) := "01";
+     variable address_var:  std_logic_vector(4 downto 0) := "00000";
+     constant one       :   std_logic_vector(4 downto 0) := "00001";
+     variable reading   :   std_logic := '0';
+     variable read_cnt  :   integer range 0 to 32;
+     variable wait_cnt  :   integer range 0 to 20;
+    ------------------------------------------------------------------------------
+    begin
+        if rising_edge(i_Clk) then
+            if ena_shift_reg = rising then
+                reading := '1';
+                address_var := "00000";
+            end if;
+            if reading = '1' then
+                if wait_cnt = 3 then
+                    if read_cnt < to_integer(unsigned(i_num_points)) then
+                        s_data_out <= data_in;
+                    else 
+                        s_data_out <= (others => '0');
+                    end if;
+                    wait_cnt := wait_cnt + 1;
+                elsif wait_cnt = 4 then
+                    s_bram_wr <= "1111";
+                    wait_cnt := wait_cnt + 1;
+                elsif wait_cnt = 6 then
+                    s_bram_wr <= "0000";
+                    wait_cnt := wait_cnt + 1;
+                elsif wait_cnt = 7 then
+                    wait_cnt := 0;
+                    read_cnt := read_cnt + 1;
+                    address_var := std_logic_vector(unsigned(address_var) + unsigned(one));
+                else
+                    wait_cnt := wait_cnt + 1;
+                end if;
+                if read_cnt = 32 then
+                    reading := '0';
+                    read_cnt := 0;
+                end if;
+            end if;
+            s_address_out <= address_var;
+        end if;
+    ------------------------------------------------------------------------------
+    end process fill_BRAM;
+    ------------------------------------------------------------------------------
+
+
+
+    ------------------------------------------------------------------------------
+    ena_shift_reg_process :   process(irq_in, i_Clk)  
+    ------------------------------------------------------------------------------
+    -- Holds the edge state of irq_in and i_new_set.
+    ------------------------------------------------------------------------------
+    begin  
+    ------------------------------------------------------------------------------
+        if rising_edge(i_Clk) then
+            ena_shift_reg <= ena_shift_reg(0) & irq_in;
+        end if;
+    ------------------------------------------------------------------------------
+    end process ena_shift_reg_process;
+    ------------------------------------------------------------------------------
+
+
+    o_address               <=  s_address_out;
+    bram_addr(31 downto 7)  <=  (30 => '1', others => '0');
+    bram_addr(6 downto 0)   <=  s_address_out & "00";
     bram_en                 <=  '1';
-    bram_dout               <=  data_in(127 downto 96); 
-    bram_wr                 <=  irq_in & irq_in & irq_in & irq_in;
+    bram_dout               <=  s_data_out(127 downto 96); 
+    bram_wr                 <=  s_bram_wr;
 
 end Behavioral;
-
